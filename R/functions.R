45cc8b029b1711429cc49e780942ad4d9d77f9d5
BenBarnard <ben_barnard@baylor.edu>
2016-06-13 00:37:45 -0500

    update on read_clean

--- R/functions.R
+++ R/functions.R
@@ -1,17 +1,45 @@
+#' Title
+#'
+#' @param dir
+#'
+#' @return
+#' @export
+#' @importFrom %>% magrittr
+#' @importFrom llply plyr
+#' @importFrom ldply plyr
+#' @importFrom read_csv readr
+#' @importFrom read_excel readxl
+#' @importFrom str_subset stringr
+#' @importFrom str_detect stringr
+#' @importFrom rename dplyr
+#' @importFrom select dplyr
+#'
+#'
+#' @examples
 read_clean <- function(dir){
   wd <- getwd()
   setwd(dir)
   files <- list.files()
-  excel_files <- plyr::llply(stringr::str_subset(files, ".xls"), readxl::read_excel)
-  csv_files <- plyr::llply(stringr::str_subset(files, ".csv"), readr::read_csv)
+
+  excel_files <- files %>%
+    str_subset(".xls") %>%
+    llply(read_excel)
+
+  csv_files <- files %>%
+    str_subset(".csv") %>%
+    llply(read_csv)
+
   all_files <- c(excel_files, csv_files)
   browser()
-  unique_df_names <- unique(plyr::llply(all_files, names))
-  suppressWarnings(plyr::llply(unique_df_names, function(x, y){
-    matches <- plyr::ldply(seq(length(y)), function(z, y, x){
-      if(all(length(names(y[[z]]) == length(x))) && all(names(y[[z]]) == x)){1}else{0}
-    }, y = y, x = x)
-    dplyr::tbl_df(plyr::ldply(y[as.logical(matches$V1)]))
-  }, y = all_files))
+
+  all_files %>% ldply(function(all_files){
+    browser()
+  degree_program_count <- all_files[str_detect(all_files$ID, "[:alpha:]"),] %>%
+    select(ID, NAME, TERM) %>%
+    rename(Degree = ID, Program = NAME, Count = TERM)
+
+  apps <- all_files[!(str_detect(all_files$ID, "[:alpha:]")),]
+  })
   setwd(wd)
 }
+
