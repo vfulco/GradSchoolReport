3bbd118a67a07ff80812a8e4d58b214215b54507
BenBarnard <ben_barnard@baylor.edu>
2017-01-10 11:00:23 -0600

    update

---
+++ R/BearhausApplicationClean.R
@@ -0,0 +1,28 @@
+#' Bearhaus Application Cleaning Function
+#'
+#' Given the data this function should remove blank columns that don't
+#' have any data. The function should also fix any data irregularities
+#' and should combine lists of data. This function is not going to read
+#' in data.
+#'
+#' @param x data in a list or data frame
+#' @param ... other options
+#'
+#' @return data frame
+#' @export
+#'
+BearhausApplicationClean <- function(x, ...){
+  UseMethod("BearhausApplicationClean")
+}
+
+#' @rdname BearhausApplicationClean
+#' @export
+BearhausApplicationClean.list <- function(x, ...){
+
+}
+
+#' @rdname BearhausApplicationClean
+#' @export
+BearhausApplicationClean.data.frame <- function(x, ...){
+
+}
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(GradSchoolReport)
+
+test_check("GradSchoolReport")
