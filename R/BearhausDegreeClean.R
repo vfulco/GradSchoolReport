8b89a9d30457ab7ce7c92e5bc80dd386a1ae8980
BenBarnard <ben_barnard@baylor.edu>
2017-01-10 15:59:11 -0600

    update

--- R/BearhausDegreeClean.R
+++ R/BearhausDegreeClean.R
@@ -1,9 +1,8 @@
 #' Bearhaus Degree Cleaning Function
 #'
-#' Given the data this function should remove blank columns that don't
-#' have any data. The function should also fix any data irregularities
-#' and should combine lists of data. This function is not going to read
-#' in data.
+#' Given the data, this function should remove blank columns that don't have any data.
+#' The function should also fix any data irregularities, and should combine lists of
+#' data. This function is not going to read in data.
 #'
 #' @param x data in a list or data frame
 #' @param ... other options
