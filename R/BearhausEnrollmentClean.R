5bb298cf7aa6a093f4d15b22a65634bdc8460aa1
BenBarnard <ben_barnard@baylor.edu>
2017-01-10 15:59:14 -0600

    update

--- R/BearhausEnrollmentClean.R
+++ R/BearhausEnrollmentClean.R
@@ -1,9 +1,9 @@
 #' Bearhaus Enrollment Cleaning Function
 #'
-#' Given the data this function should remove blank columns that don't
-#' have any data. The function should also fix any data irregularities
-#' and should combine lists of data. This function is not going to read
-#' in data.
+#' Given the data, this function should remove blank columns that don't have any data.
+#' The function should also fix any data irregularities, and should combine lists of
+#' data. This function is not going to read in data.
+#'
 #'
 #' @param x data in a list or data frame
 #' @param ... other options
