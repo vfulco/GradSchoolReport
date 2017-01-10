a7e8c4b3869ee6048e18bfd845fe237bac282c0a
BenBarnard <ben_barnard@baylor.edu>
2017-01-10 11:01:05 -0600

    update

---
+++ vignettes/Installation_and_Use.R
@@ -0,0 +1,9 @@
+## ---- eval = FALSE-------------------------------------------------------
+#  install.packages("devtools")
+
+## ---- eval = FALSE-------------------------------------------------------
+#  devtools::install_github("BenBarnard/GradSchoolReport")
+
+## ---- eval = FALSE-------------------------------------------------------
+#  GradSchoolReport::runGradSchoolApp()
+
