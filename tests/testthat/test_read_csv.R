514a17fc5e50b02c4971ff1f6e74e98662d61f3f
BenBarnard <ben_barnard@baylor.edu>
2017-01-11 15:15:56 -0600

    added coverage

--- tests/testthat/test_read_csv.R
+++ tests/testthat/test_read_csv.R
@@ -1,9 +1,20 @@
 context("read_csv")
 
 test_that("read_csv reads_list and combines", {
-  expect_equal(read_csv(list("a,b\n1.0,2.0", "a,b\n1.0,2.0"), combine = TRUE), data.frame(a = c(1, 1), b = c(2, 2)))
+  expect_equal(read_csv(list("a,b\n1.0,2.0", "a,b\n1.0,2.0"),
+                        combine = TRUE),
+               data.frame(a = c(1, 1), b = c(2, 2)))
 })
 
 test_that("read_csv reads_list and combines", {
-  expect_equal(read_csv(list("a,b\n1.0,2.0", "a,b\n1.0,2.0")), list(tibble::data_frame(a = c(1), b = c(2)), tibble::data_frame(a = c(1), b = c(2))))
+  expect_equal(read_csv(list("a,b\n1.0,2.0", "a,b\n1.0,2.0"),
+                        combine = FALSE),
+               list(tibble::data_frame(a = c(1), b = c(2)),
+                    tibble::data_frame(a = c(1), b = c(2))))
+})
+
+
+test_that("read_csv default", {
+  expect_equal(read_csv("a,b\n1.0,2.0"),
+               tibble::data_frame(a = c(1), b = c(2)))
 })
