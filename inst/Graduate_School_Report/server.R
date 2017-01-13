161c116fe4d89ef1dd5e73579b996e9d8cb5f91d
BenBarnard <ben_barnard@baylor.edu>
2017-01-13 15:56:20 -0600

    update

--- inst/Graduate_School_Report/server.R
+++ inst/Graduate_School_Report/server.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(shinyFiles)
+library(plyr)
 library(rmarkdown)
 library(GradSchoolReport)
 
@@ -9,16 +10,51 @@ shinyServer(function(input, output, session){
 
   dir <- reactive(input$dir)
 
-
   # path
   path <- reactive({
-    home <- normalizePath("~")
-    file.path(home, paste(unlist(dir()$path[-1]), collapse = .Platform$file.sep))
+    if(is.null(input$dir)){
+      return()
+    }else{
+      home <- normalizePath("~")
+      path <- file.path(home, paste(unlist(dir()$path[-1]), collapse = .Platform$file.sep))
+      templates <- system.file("Graduate_School_Report/templates",
+                               package = "GradSchoolReport")
+      l_ply(as.list(list.files(templates, full.names = TRUE)),
+            file.copy, to = paste0(path, "/reports"), overwrite = TRUE)
+      l_ply(as.list(list.files(templates, pattern = ".Rmd", full.names = TRUE)), render, params = list(path = path), quiet = TRUE)
+      l_ply(as.list(list.files(templates, pattern = ".Rmd", full.names = TRUE)), file.remove)
+      path
+    }
   })
 
   # files
-  output$files <- renderPrint(list.files(path()))
+  output$files <- renderPrint({
+    if(is.null(input$dir)){
+      return()
+    }else{
+    list.files(path())
+    }
+    })
 
+  output$dir <- renderPrint({
+    if(is.null(input$dir)){
+      return()
+    }else{
+      path()
+    }
+
+
+    output$inc<-renderUI({
+      if(is.null(path())){
+        return()
+      }else{
+        tabsetPanel(type = "tabs",
+                    tabPanel("Graduate School Charts", includeHTML(paste0(path(), "/reports/GraduateSchoolCharts.html")))
+        )
+      }
+    })
+
+  })
 
 
 
