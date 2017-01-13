6c6a87fc04ccfe871d748b00052a69d148a4e128
BenBarnard <ben_barnard@baylor.edu>
2017-01-13 16:04:05 -0600

    fixed test ish

--- tests/testthat/test_BearhausApplicationClean.R
+++ tests/testthat/test_BearhausApplicationClean.R
@@ -1,10 +1,10 @@
 context("BearhausApplicationClean")
 
 test_that("BearhausApplicationClean converts data.frame", {
-  expect_equal(BearhausApplicationClean.data.frame(iris), iris)
+  expect_equal(is.data.frame(BearhausApplicationClean.data.frame(iris)), is.data.frame(iris))
 })
 
 test_that("BearhausApplicationClean converts data.frame", {
-  expect_equal(BearhausApplicationClean(list(iris, iris)), rbind(iris, iris))
+  expect_equal(is.data.frame(BearhausApplicationClean(list(iris, iris))), is.data.frame(rbind(iris, iris)))
 })
 
