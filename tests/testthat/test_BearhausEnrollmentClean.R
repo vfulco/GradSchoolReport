3fd12c73ebfd434d72b4004892d73e6bac6cd84f
BenBarnard <ben_barnard@baylor.edu>
2017-01-13 16:14:47 -0600

    update

--- tests/testthat/test_BearhausEnrollmentClean.R
+++ tests/testthat/test_BearhausEnrollmentClean.R
@@ -1,10 +1,10 @@
 context("BearhausEnrollmentClean")
 
 test_that("BearhausEnrollmentClean converts data.frame", {
-  expect_equal(BearhausEnrollmentClean.data.frame(iris), iris)
+  expect_equal(BearhausEnrollmentClean(iris, test = TRUE), iris)
 })
 
 test_that("BearhausEnrollmentClean converts data.frame", {
-  expect_equal(BearhausEnrollmentClean(list(iris, iris)), rbind(iris, iris))
+  expect_equal(BearhausEnrollmentClean(list(iris, iris), test = TRUE), rbind(iris, iris))
 })
 
