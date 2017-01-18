8fc6951eae7dfbc0b019ea6b7c51f3bdcf024e32
BenBarnard <ben_barnard@baylor.edu>
2017-01-18 14:38:12 -0600

    dplyr::

--- R/BearhausEnrollmentClean.R
+++ R/BearhausEnrollmentClean.R
@@ -30,6 +30,7 @@ BearhausEnrollmentClean.list <- function(x, ...){
 #' @importFrom dplyr select
 #' @importFrom dplyr rename
 #' @importFrom stringr str_extract
+#' @importFrom forcats fct_relevel
 BearhausEnrollmentClean.data.frame <- function(x, ...){
   dots <- lazyeval::lazy_dots(...)
   x <- x[,!(sapply(x, function(y){
@@ -37,6 +38,7 @@ BearhausEnrollmentClean.data.frame <- function(x, ...){
     }))]
   x <- select(x, -c(`GRE Quan Percent`, `GRE Verbal Percent`, `GRE Quant REV Percent`, GRE_Verbal_REV_Percent, `GRE Writing Percent`))
   x <- rename(x, `GRE Quantitative` = `GRE HI Quantitative`, `GRE Verbal` = `GRE HI Verbal`, `Academic Year` = Year)
-  x$`Year` <- str_extract(x$Term, "[0-9]{4}")
+  x$`Year` <- as.numeric(str_extract(x$Term, "[0-9]{4}"))
+  x$`Term` <- as.ordered(interaction(fct_relevel(as.factor(x$`Term Flag`), "Spring", "Summer", "Fall"), x$Year, sep = " "))
   x
 }
